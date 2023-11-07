ad77e65e72e7e478ee1f699c11c4cc9004b3b892
kushaltatariya <103580859+kushaltatariya@users.noreply.github.com>
2023-11-07 16:56:17 +0100

    Updating the slurm script for vsc

--- source/slurm_example.sh
+++ source/slurm_example.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cluster=wice #NOTE this is only if you are on the WICE cluster, skip this if you are on Genius
+#SBATCH -N 1
+#SBATCH --ntasks=18
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=gpu
 #SBATCH --time=02:00:00
 #SBATCH --A intro_vsc35000 #NOTE you need to put your own login name here!
 
 cd $VSC_DATA
-
-module purge
-module load foss/2021a
 
 source activate conda_env #conda_env = the name of your conda environment
 
